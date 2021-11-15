a8e02e7f82589c7fb8358c030b21af3e63ff35e2
Luca Leyva <imlucaleyva@gmail.com>
2021-11-15 16:09:31 -0500

    Set up function to displayTrip.

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
 "use strict"
 // ** STEP 1: Create arrays for destination, restauarant, transportation, and entertainment 
+
 let destinations = ['NYC', 'Philly', 'Boston', 'Upstate',];
 let resturants = ['Mexican', 'Thai', 'Indian','Ethiopian'];
 let transportion = ['Train','Car','Bike','Bus'];
 let entertainment = ['Hike','Food tour','Museum','Concert'];
+let tripFeatures = [
+    getRandomElementFromArray(destinations),
+    getRandomElementFromArray(resturants),
+    getRandomElementFromArray(transportion),
+    getRandomElementFromArray(entertainment),
+]
 
 //** STEP 2: Randomly select some elements
+
 function getRandomElementFromArray(array){
-    return array[Math.floor(Math.random() * array.length)]
+    return array[Math.floor(Math.random() * array.length)];
 }
 
-let randomDestination = getRandomElementFromArray(destinations);
-console.log(randomDestination)
+// ** STEP 3: Wrie a function to display our completed trip as an alert
 
-let randomResturant = getRandomElementFromArray(resturants);
-console.log(randomResturant)
-
-let randomTransportation = getRandomElementFromArray(transportion);
-console.log(randomTransportation)
-
-let randomEntertainment = getRandomElementFromArray(entertainment);
-console.log(randomEntertainment)
+function displayTrip(arrayOfFeatures){
+    // ** Declare a final variable to be alerted
+    let finalString = "";
+    //** Iterate through our array of features and build the final string
+    for (let index = 0; index < array.length; index++){
+        finalString += arrayOfFeatures(index);
+    }
+    // ** Alert the final string
+    alert(finalString);
+}
