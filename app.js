5bd80651e1dbf6b4d8677866eeab0e8195b02290
Luca Leyva <imlucaleyva@gmail.com>
2021-11-15 15:10:29 -0500

    Initialized project, and created day trip arrays.

---
+++ app.js
@@ -0,0 +1,8 @@
+"use strict"
+// ** STEP 1: Create arrays for destination, restauarant, transportation, and entertainment 
+let destinations = ['NYC', 'Philly', 'Boston', 'Upstate',];
+let resturants = ['Mexican', 'Thai', 'Indian','Ethiopian'];
+let transportion = ['Train','Car','Bike','Bus'];
+let entertainment = ['Hike','Food tour','Museum','Concert'];
+
+//** STEP 2: Randomly select some elements
